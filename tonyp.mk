3ad0c46776122581012c0972c01e000703c41462
tonyp <tonyptonyp@gmail.com>
2013-09-09 16:21:15 +0200

    move tonyp stuff from vendor/cm to vendor/tonyp

--- tonyp.mk
+++ tonyp.mk
@@ -1,3 +1,21 @@
+# tonyp version
+TONYP_VERSION = 07
+
+PRODUCT_PROPERTY_OVERRIDES += \
+  ro.tonyp.version=$(TONYP_VERSION)
+
+# prebuilts
+PRODUCT_COPY_FILES +=  \
+    vendor/tonyp/prebuilt/BarcodeScanner.apk:system/app/BarcodeScanner.apk
+
+# enable pipe
+TARGET_USE_PIPE := true
+
+# hybrid engine
+PRODUCT_COPY_FILES += \
+    vendor/tonyp/prebuilt/55-tonyp.sh:system/addon.d/55-tonyp.sh \
+    vendor/tonyp/prebuilt/pa_hdpi.conf:system/etc/beerbong/properties.conf
+
 # Enable mini gapps
 MINI_GAPPS := true
 $(call inherit-product, vendor/google/tiny_gapps_nonneon_tonyp.mk)
